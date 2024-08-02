bb99ae9d7115560fd70f4ed1f5ea4183287790b8
hshehriyaralam <hshehriyaralam@gmail.com>
2024-08-02 05:03:49 -0600

    Union Type in TypeScript

---
+++ 07) Union Types in Typescript/app.js
@@ -0,0 +1,24 @@
+// Union Types 
+// jab hamien multiple Data type store krna ho to
+// Union main 2 ya 2 zyada Type kr sakte hain
+// store  userId
+var userId = "ghtyvun56nuu9771";
+userId = 892364682563;
+userId = "89542bkcgoigst9fweby";
+userId = 7984375634;
+userId = true;
+// Display and return userId
+function Display(userId) {
+    console.log("User Id is " + userId);
+    return userId;
+}
+/*
+************************************** Summary **************************************
+01) Multiple Type assign is called Union Type
+02) Union Type method use only combined method
+03) Parameters allow to union type
+
+
+
+
+*/ 
